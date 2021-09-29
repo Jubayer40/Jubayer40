365cb4cd0ac68a98744496943f5e7d59c03da626
Jubayer40 <91624759+Jubayer40@users.noreply.github.com>
2021-09-29 22:56:43 +0600

    Add files via upload

---
+++ Even number.c
@@ -0,0 +1,13 @@
+//Even
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=2; i <= 100; i++)
+    {
+
+        printf("%d\n", i);
+    }
+    return 0;
+}
+
---
+++ Loop.c
@@ -0,0 +1,12 @@
+// For loop
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1; i <= 100; i++)
+    {
+        printf("C Programming\n");
+        printf("%d\n", i);
+    }
+    return 0;
+}
