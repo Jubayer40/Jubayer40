a32d4c02e5f9be8f419159fc20804b0296f2a99f
Jubayer40 <91624759+Jubayer40@users.noreply.github.com>
2022-01-06 01:11:04 +0600

    Add files via upload

---
+++ pattern  triangle 100111.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int row, col, y;
+    printf("Enter y= ");
+    scanf("%d",&y);
+
+    for(row=1;row<=y;row++)
+    {
+        for(col=1;col<=row;col++)
+            printf("%c",col+64);
+        printf("\n");
+    }
+    return 0;
+}
+
---
+++ pattern  triangle 110101.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int row, col, y;
+    printf("Entery= ");
+    scanf("%d",&y);
+
+    for(row=1;row<=y;row++)
+    {
+        for(col=1;col<=row;col++)
+            printf("%d",col%2);
+        printf("\n");
+    }
+    return 0;
+}
